fe040532ad6c70984cd2ff5e74a1a7c41689076e
Bosyaf <boss104yaf@gmail.com>
2026-04-07 19:19:45 +0300

    Delete kernel directory

--- kernel/sched/walt/trace_penalty.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-only
-/*
- * Copyright (c) 2023, Qualcomm Innovation Center, Inc. All rights reserved.
- */
-
-#include "walt.h"
-
-#define CREATE_TRACE_POINTS
-#include "trace_penalty.h"
